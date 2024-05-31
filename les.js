763fecfc0c1d4025a1890c71fdc442f94eb2ac28
AldairLuzuriaga <aldairleon27@gmail.com>
2024-05-30 19:07:35 -0500

    Add files via upload

---
+++ les.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        alert('¡Hola! Has presionado el botón.');
+    });
+});
